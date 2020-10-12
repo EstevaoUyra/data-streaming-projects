0c1a950bb838caf4cb2cac3b3700d363f4de02ea
Estêvão Uyrá P. V <estevao.uyra.pv@gmail.com>
2020-10-12 08:56:10 -0300

    Initial commit

---
+++ sf-crime-data-project-files/kafka_server.py
@@ -0,0 +1,22 @@
+import producer_server
+from CONSTANTS import BOOTSTRAP_SERVER
+
+def run_kafka_server():
+    input_file = "./police-department-calls-for-service.json"
+
+    producer = producer_server.ProducerServer(
+        input_file=input_file,
+        topic="police.calls.service",
+        bootstrap_servers=BOOTSTRAP_SERVER,
+        client_id="producer.calls_for_service"
+    )
+    return producer
+
+
+def feed():
+    producer = run_kafka_server()
+    producer.generate_data()
+
+
+if __name__ == "__main__":
+    feed()
